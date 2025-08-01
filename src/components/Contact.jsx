b20704b1cda30761f10e52e08958f6c29299c634
AryanBhardwajIndia <aryanbhardwaj767@gmail.com>
2025-08-01 21:46:45 +0530

    Code Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="contact-section">
       <h1 className="contact-heading">Contact Me</h1>
-      <p className="contact-email"><a href="mailto:aryanbhardwaj767@gmail.com">aryanbhardwaj767@gmail.com</a></p>
+      <p className="contact-email"><span className="line-below"><a href="mailto:aryanbhardwaj767@gmail.com">aryanbhardwaj767@gmail.com</a></span></p>
     </section>
   );
 };
