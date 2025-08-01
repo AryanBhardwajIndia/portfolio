12213a238d032272d18c5b4a0d8cdc808d2764f8
AryanBhardwajIndia <aryanbhardwaj767@gmail.com>
2025-08-01 16:17:10 +0530

    Updated Code

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,15 @@
 import "./Skills.css";
 
 const skills = [
-  { name: "C", icon: "./c.png" },
-  { name: "C++", icon: "./cpp.png" },
-  { name: "JavaScript", icon: "./js.png" },
-  { name: "Python", icon: "./python.webp" },
-  { name: "HTML", icon: "./html.png" },
-  { name: "CSS", icon: "./css.png" },
-  { name: "PHP", icon: "./php.png" },
-  { name: "MySQL", icon: "./mysql.png" },
-  { name: "Kotlin", icon: "./kotlin.png" },
+  { name: "C", icon: "/c.png" },
+  { name: "C++", icon: "/cpp.png" },
+  { name: "JavaScript", icon: "/js.png" },
+  { name: "Python", icon: "/python.webp" },
+  { name: "HTML", icon: "/html.png" },
+  { name: "CSS", icon: "/css.png" },
+  { name: "PHP", icon: "/php.png" },
+  { name: "MySQL", icon: "/mysql.png" },
+  { name: "Kotlin", icon: "/kotlin.png" },
 ];
 
 const Skills = () => {
