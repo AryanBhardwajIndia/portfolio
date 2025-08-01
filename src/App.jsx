153116d6edbd6e2d50d76a908370efa1e5447ea0
AryanBhardwajIndia <aryanbhardwaj767@gmail.com>
2025-08-01 21:06:49 +0530

    Updated Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Project from "./components/Projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-const App = () => {
+function App() {
   return (
-    <div className="main-wrapper">
+    <>
       <Hero />
       <About />
       <Skills />
-      <Project/>
-      <Contact/>
-    </div>
+      <Projects />
+      <Contact />
+    </>
   );
-};
+}
 
 export default App;
